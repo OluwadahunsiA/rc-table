7248a2c7273fc53f3a9b1c6f57208fe20f804f6d
OluwadahunsiA <tommydate0@gmail.com>
2023-11-23 12:34:05 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,119 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useEffect, useState } from 'react';
+import qs from 'qs';
+import Table from '.';
+
+interface DataType {
+  name: {
+    first: string;
+    last: string;
+  };
+  gender: string;
+  email: string;
+  login: {
+    uuid: string;
+  };
+}
+
+interface TableParams {
+  pagination?: any;
+  sortField?: string;
+  sortOrder?: string;
+  filters?: Record<string, any>;
+}
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: true,
+    render: (name: { first: any; last: any }) => `${name.first} ${name.last}`,
+    width: '20%',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    filters: [
+      { text: 'Male', value: 'male' },
+      { text: 'Female', value: 'female' },
+    ],
+    width: '20%',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+];
+
+const getRandomuserParams = (params: TableParams) => ({
+  results: params.pagination?.pageSize,
+  page: params.pagination?.current,
+  ...params,
+});
+
+const sample = () => {
+  return <div>Is this a react node</div>;
+};
+
+const App: React.FC = () => {
+  const [data, setData] = useState<DataType[]>();
+  const [loading, setLoading] = useState(false);
+  const [tableParams, setTableParams] = useState<TableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+
+  const fetchData = () => {
+    setLoading(true);
+    fetch(
+      `https://randomuser.me/api?${qs.stringify(
+        getRandomuserParams(tableParams)
+      )}`
+    )
+      .then((res) => res.json())
+      .then(({ results }) => {
+        setData(results);
+        setLoading(false);
+        setTableParams({
+          ...tableParams,
+          pagination: {
+            ...tableParams.pagination,
+            total: 200,
+            // 200 is mock data, you should read it from server
+            // total: data.totalCount,
+          },
+        });
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [JSON.stringify(tableParams)]);
+
+  // const handleTableChange = (pagination: any, filters: any, sorter: any) => {
+  //   setTableParams({
+  //     pagination,
+  //     filters,
+  //     ...sorter,
+  //   });
+
+  //   // `dataSource` is useless since `pageSize` changed
+  //   if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+  //     setData([]);
+  //   }
+  // };
+
+  return (
+    <Table
+      columns={columns}
+      data={data}
+      caption={sample()}
+  
+    />
+  );
+};
+
+export default App;
---
+++ src/Footer/SummaryContext.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as React from 'react';
+import type { ColumnsType, ColumnType, StickyOffsets } from '../interface';
+
+type FlattenColumns<RecordType> = readonly (ColumnType<RecordType> & { scrollbar?: boolean })[];
+
+const SummaryContext = React.createContext<{
+  stickyOffsets?: StickyOffsets;
+  scrollColumnIndex?: number;
+  flattenColumns?: FlattenColumns<any>;
+  columns?: ColumnsType<any>;
+}>({});
+
+export default SummaryContext;
